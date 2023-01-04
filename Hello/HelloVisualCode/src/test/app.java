63c0edd8d6db5a2bd9ccc26f9a4908f26719bb62
ThanhNhiet <nhiethiz@gmail.com>
2023-01-04 16:59:17 +0700

    lan3

---
+++ Hello/HelloVisualCode/src/test/app.java
@@ -0,0 +1,8 @@
+package test;
+
+public class app {
+        public static void main(String[] args) throws Exception {
+            String name = "Pham Le Thanh Nhiet";
+            System.out.println("Hello, "+name);
+        }
+}
